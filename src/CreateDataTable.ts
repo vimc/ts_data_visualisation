9502c0ad4ddfba8149b333a6c7836618238da226
james thompson <james.thompson@imperial.ac.uk>
2018-08-10 15:44:26 +0100

    Save data as a wide dataframe

--- src/CreateDataTable.ts
+++ src/CreateDataTable.ts
@@ -6,10 +6,23 @@ export class TableMaker {
         for (let ds of dataSets) {
             const label = ds.label
             for (let i = 0; i < compareNames.length; ++i) {
-                const compName = compareNames[i]
-                const dataValue = ds.data[i]
+                const compName = compareNames[i];
+                const dataValue = ds.data[i];
                 returnData.push({compare: compName, label: label, data: dataValue})
             }
+        }
+        return returnData;
+    }
+
+    createWideTable(dataSets: any[], compareNames: string[]) : KnockoutObservableArray<any> {
+        let returnData = ko.observableArray([]);
+        const disAggVars = dataSets.map(x => x['label'])
+        for (let i = 0; i < compareNames.length; ++i) {
+            let tempObject: any = {compare: compareNames[i]};
+            for (let ds of dataSets) {
+                tempObject[ds.label] =  ds.data[i];
+            }
+            returnData.push(tempObject);
         }
         return returnData;
     }
