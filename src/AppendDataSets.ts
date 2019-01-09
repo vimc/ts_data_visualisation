e28f82b050ae047b0f4cb7874e58b149abc51ccc
james thompson <james.thompson@imperial.ac.uk>
2019-01-09 12:34:12 +0000

    Remove the debugging code

--- src/AppendDataSets.ts
+++ src/AppendDataSets.ts
@@ -25,11 +25,6 @@ export function appendToDataSet(touchstones: string[],
         if (seenDataSets.indexOf(touchstone) == -1) {
             // ...if not add it
             const filename = "./data/impactData_" + touchstone + ".js"
-            console.log(filename);
-/*            const newData = $.get(filename, function(data: ImpactDataRow[]) :ImpactDataRow[] {
-              return data;
-            }, "script");*/
-
             let newData: ImpactDataRow[] = curDataset; // fail safe in case the file can't be read
             $.ajax({
                 'async': false,
@@ -40,29 +35,15 @@ export function appendToDataSet(touchstones: string[],
                 'url': filename,
                 'data': { 'request': "", 'target': 'arrange_url', 'method': 'method_target' },
                 'success': function (data: string): void {
-//                    console.log(filename)
-//                    console.log(data)
-//                    const temp = eval(data);
-//                    console.log(temp.length)
-//                    console.log(temp[0])
                     newData = eval(data);
                     return;
                 }
             });
 
-/*            var x = new XMLHttpRequest();
-            x.open('GET', filename);
-            x.onreadystatechange = function() {
-              alert(x.responseText);
-            }
-            x.send();*/
             curDataset = curDataset.concat(newData);
             seenDataSets.push(touchstone);
-
-            console.log(newData.length)
         }
     }
-
     const update = {
         newSeenList: seenDataSets,
         newDataSet: curDataset
