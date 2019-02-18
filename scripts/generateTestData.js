82407d1d15c922e36c7c40c92e8cce60b2818b18
james thompson <james.thompson@imperial.ac.uk>
2019-02-18 12:37:12 +0000

    Update testing script

--- scripts/generateTestData.js
+++ scripts/generateTestData.js
@@ -10,6 +10,16 @@ fs.writeFile("data/test/impactData.js", generateData(), function (err) {
     console.log("Fake data saved to data/test/impactData.js")
 });
 
+for (let i in touchstones) {
+    var fileName = "data/test/impactData_" + touchstones[i] + ".js";
+    fs.writeFile(fileName, generateData(), function (err) {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log("Fake data saved to " + fileName)
+});
+}
 
 function generateData() {
 
