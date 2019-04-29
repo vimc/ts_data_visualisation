8d04f946fb9a7c7ea4a35bc0aaa9cbd317315415
james thompson <james.thompson@imperial.ac.uk>
2019-04-29 10:55:21 +0100

    add test for chart configs

--- tests/ChartConfigTests.ts
+++ tests/ChartConfigTests.ts
@@ -1,5 +1,9 @@
-import {annotationHelper, dateToAnnotation, BaseAnnotation, CustomChartOptions} from "../src/Chart";
+import {ChartConfiguration} from "chart.js";
+import {annotationHelper, dateToAnnotation, impactChartConfig,
+        timeSeriesChartConfig, AnnotatedChartConfiguration,
+        BaseAnnotation, CustomChartOptions} from "../src/Chart";
 import {touchstones} from "../src/Data";
+import {FilteredData} from "../src/DataFilterer";
 import {expect} from "chai";
 
 describe("ChartConfigs", () => {
@@ -10,7 +14,6 @@ describe("ChartConfigs", () => {
     expect(conf.value).to.equal(1066);
     expect(conf.borderColor).to.equal("xyz");
     expect(conf.label.content).to.equal("abc");
-    console.log(conf);
   })
 
   it("Check dateToAnnotation", () => {
@@ -22,8 +25,72 @@ describe("ChartConfigs", () => {
     // are the same as the ones we passed in.
     expect(t).to.eql(touchstones);
   })
+
+  it("Check impactChartConfig", () => {
+    const c: CustomChartOptions
+        = <CustomChartOptions>{
+            activityTypes: ["at1", "at2"],
+            xAxis: "cofinance_status_2018",
+            cumulative: true,
+            yAxis: "DIS",
+            hideLabels: false,
+            maxPlot: 163,
+            metric: "METRIC",
+            plotTitle: "TITLE",
+            plotType: "TYPE",
+            selectedCountries: ["COD", "MMR"], 
+            selectedTouchstones: ["ts1", "ts2", "ts3", "ts4", "ts5", "ts6"],
+            selectedVaccines: ["HepB", "HepB_BD", "Hib3", "HPV", "JE", "MCV1", "MCV2"],
+            timeSeries: true,
+            yAxisTitle: "AXIS TITLE",
+            yearHigh: 1066,
+            yearLow: 2525,
+        };
+    // filtered data is tested elsewhere
+    const f: FilteredData = {datasets: [],
+                             xAxisVals: [""],
+                             totals: [0]};
+
+    const config: ChartConfiguration =
+      impactChartConfig(f, ["a", "b", "c"], c);
+
+    expect(config.data.labels).to.eql(["a", "b", "c"]);
+    expect(config.options.title.text).to.eql(c.plotTitle);
+    expect(config.options.scales.yAxes[0].scaleLabel.labelString).
+      to.eql(c.yAxisTitle)
+  })
+
+  it("Check timeSeriesChartConfig", () => {
+    const c: CustomChartOptions
+        = <CustomChartOptions>{
+            activityTypes: ["at1", "at2"],
+            xAxis: "cofinance_status_2018",
+            cumulative: true,
+            yAxis: "DIS",
+            hideLabels: false,
+            maxPlot: 163,
+            metric: "METRIC",
+            plotTitle: "TITLE",
+            plotType: "TYPE",
+            selectedCountries: ["COD", "MMR"], 
+            selectedTouchstones: ["ts1", "ts2", "ts3", "ts4", "ts5", "ts6"],
+            selectedVaccines: ["HepB", "HepB_BD", "Hib3", "HPV", "JE", "MCV1", "MCV2"],
+            timeSeries: true,
+            yAxisTitle: "AXIS TITLE",
+            yearHigh: 1066,
+            yearLow: 2525,
+        };
+    // filtered data is tested elsewhere
+    const f: FilteredData = {datasets: [],
+                             xAxisVals: [""],
+                             totals: [0]};
+
+    const config: AnnotatedChartConfiguration =
+      timeSeriesChartConfig(f, ["a", "b", "c"], c);
+
+    expect(config.data.labels).to.eql(["a", "b", "c"]);
+    expect(config.options.title.text).to.eql(c.plotTitle);
+    expect(config.options.scales.yAxes[0].scaleLabel.labelString).
+      to.eql(c.yAxisTitle)
+  })
 });
-
-
-//impactChartConfig
-//timeSeriesChartConfig
