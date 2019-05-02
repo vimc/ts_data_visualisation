7a625ad6efd779e6e296bbdef82b958ee9481d8b
james thompson <james.thompson@imperial.ac.uk>
2019-05-02 15:08:48 +0100

    update to use latest dataset

--- src/Dictionaries.ts
+++ src/Dictionaries.ts
@@ -155,12 +155,12 @@ export const diseaseVaccineLookup: { [code: string]: string[]} = {
 
 export const touchstoneYears: { [code: string]: number} = {
     "201210gavi-201303gavi" : 2011,
-    "201210gavi-201807wue"  : 2017,
+    "201210gavi-201810gavi" : 2017,
     "201310gavi"            : 2012,
     "201310gavi-201403gavi" : 2012,
-    "201310gavi-201807wue"  : 2017,
+    "201310gavi-201810gavi" : 2017,
     "201510gavi"            : 2014,
-    "201510gavi-201807wue"  : 2017,
+    "201510gavi-201810gavi" : 2017,
     "201710gavi"            : 2016,
-    "201710gavi-201807wue"  : 2017,
+    "201710gavi-201810gavi" : 2017,
 };
