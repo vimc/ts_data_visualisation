247fa50b91d55d3f9e13892d654843018ed16a9e
james thompson <james.thompson@imperial.ac.uk>
2019-12-06 12:47:04 +0000

    Add flag to move switch between modes

--- src/MetricsAndOptions.ts
+++ src/MetricsAndOptions.ts
@@ -1,4 +1,5 @@
 export interface MetricsAndOptions {
+    mode: "public" | "private"
     // These are the metric columns in the data set e.g. deaths, dalys, cases_averted etc.
     metrics: string[];
     // These are the methods we can toggle between
