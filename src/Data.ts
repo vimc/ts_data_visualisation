c49f36273f83909e481f4feeb4d09e787b91bc90
james thompson <james.thompson@imperial.ac.uk>
2020-02-05 12:18:39 +0000

    Add documentation for Data.ts

--- src/Data.ts
+++ src/Data.ts
@@ -1,3 +1,13 @@
+/*
+ * This file reads the data files into javascript objects using the 
+ * loadObjectFromJSONFile. There is nothing clever going on here. If there are
+ * problems withany of these objects first check that the files exist in the
+ * same directory as the app. Then check loadObjectFromJSONFile.
+ *
+ * We could infer these by examining the dataset - but this is problematic since
+ * we do not load the entire dataset when the app launches so the lists might be
+ * incomplete which would be difficult to recover from later.
+*/
 import {ImpactDataRow} from "./ImpactDataRow";
 import {MetricsAndOptions} from "./MetricsAndOptions";
 import {loadObjectFromJSONFile} from "./Utils";
@@ -11,6 +21,16 @@ export const supportTypes: string[] =
 export const activityTypes: string[] =
   loadObjectFromJSONFile("./activities.json");
 
+/*
+ * Since diseases and vaccines are so closely coupled we don't keep seperate
+ * files with both in. Instead we have a file of the form
+ * {
+ *  diseaseA : [vaccineA1, vaccineA2],
+ *  diseaseB : [vaccineB1],
+ *  diseaseC : [vaccineC1, vaccineC2, vaccineC3]
+ * }
+ * We then parse this into seperate lists of diseases and vaccines
+ */
 export const disVac = loadObjectFromJSONFile("./diseaseVaccines.json");
 export const diseases: string[] = [];
 export const vaccines: string[] = [];
@@ -19,27 +39,11 @@ for (const k in disVac) {
   vaccines.concat(disVac[k]);
 }
 
+// this will be of the form {"min":[2000],"max":[2018]}
+// has to be an array of numbers to get around some strange javascript behaviour
 export const dates: {[code: string]: number[]} =
   loadObjectFromJSONFile("./dates.json");
 
-/*
-This function is not used - the idea was to generate country groups (e.g. pine5)
-from the dataset rather than reading them from a file. However we use the
-country group before we fully load the data set and we can get into a mess.
-
-Se also the commented out line in ImpactDataRow
-
-There probably is a way to do this, but I'm not sure what it is yet.
-
-function generateCountryGroup(impactData: ImpactDataRow[],
-                id: string): string[] {
-  const countries: string[] = impactData.filter((row) => row[id])
-                      .map((row) => row.country);
-  return [...new Set(countries)];
-}
-*/
-
-// read in the country groups from files
 export const countryGroups: { [code: string]: string[] } = {
   pine: loadObjectFromJSONFile("./pine5.json"),
   dove94: loadObjectFromJSONFile("./dove94.json"),
@@ -49,7 +53,7 @@ export const countryGroups: { [code: string]: string[] } = {
   gavi77: loadObjectFromJSONFile("./gavi77.json"),
 };
 
-export const reportInfo =
+export const reportInfo: {[code: string]: string[]} =
   loadObjectFromJSONFile("./reportInfo.json");
 
 export const metricsAndOptions: MetricsAndOptions =
