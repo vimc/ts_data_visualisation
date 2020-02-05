eb80b349a6186a5a62dfb1a77f0243d62bcfe3c6
james thompson <james.thompson@imperial.ac.uk>
2020-02-05 15:35:46 +0000

    document FilteredRow.ts

--- src/FilteredRow.ts
+++ src/FilteredRow.ts
@@ -2,6 +2,7 @@ export interface FilteredRow {
   label: string;
   data: number[];
   backgroundColor: any;
-  [key: string]: any; // this is necessary to allow dynamic access object property using variable
+  // this is necessary to allow dynamic access object property using variable
   // https://stackoverflow.com/questions/32968332/
+  [key: string]: any; 
 }
